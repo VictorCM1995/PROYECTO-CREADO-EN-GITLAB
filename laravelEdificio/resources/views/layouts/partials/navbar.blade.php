535136e35186ca71daa61dfed7fef1915cd10024
Víctor Cuevas Martínez <DAW208@iesmhp.local>
2021-02-05 17:50:35 +0100

    Sin Registro

--- laravelEdificio/resources/views/layouts/partials/navbar.blade.php
+++ laravelEdificio/resources/views/layouts/partials/navbar.blade.php
@@ -16,36 +16,6 @@
       </li>
     </ul>
     {{--@endif --}}
-    @if(Auth::check() )
-        <form class="d-flex">
-          <input id="busqueda" class="form-control mr-sm-3" type="text" placeholder="Buscar" aria-label="Buscar">
-        </form>
-        
-        <ul class="navbar-nav navbar-right">
-          <li class="nav-item">
-            <a href="{{ route('profile.show') }}"  class="nav-link">
-              {{ Auth::user()->name }}
-            </a>
-          </li>
-            <li class="nav-item">
-                <a href="{{ route('logout') }}"  class="nav-link"
-                  onclick="event.preventDefault();
-                   document.getElementById('logout-form').submit();" >
-                    <span class="glyphicon glyphicon-off"></span>
-                    Cerrar sesión
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        </ul>
-    @else
-        <ul class="navbar-nav navbar-right">
-            <li class="nav-item">
-              <a href="{{url('login')}}" class="nav-link">Login</a>
-            </li>
-        </ul>
-    @endif 
   </div>
 </div>
 </nav>
